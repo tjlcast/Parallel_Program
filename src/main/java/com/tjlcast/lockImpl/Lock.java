222e8bc186ffa21c9396f16b353357a5fc72c802
tangjialiang <phx108@sina.com>
2018-01-22 23:02:37 +0800

    tjl: spring mvn的request线程安全

--- src/main/java/com/tjlcast/lockImpl/Lock.java
+++ src/main/java/com/tjlcast/lockImpl/Lock.java
@@ -9,9 +9,11 @@ public class Lock {
     private Thread lockingThread = null ;
 
     /**
+     * 线程进入由 sychronized 和 wait 进行控制
      * 这段代码中的进入lock() 并调用 wait()进行比较，
      * 会发现大部分时间用在等待进入锁和进行wait()的等待中，
      * 而不是被阻塞在试图进入lock()中
+     *
      * @throws InterruptedException
      */
     public synchronized void lock() throws InterruptedException {
@@ -33,3 +35,21 @@ public class Lock {
         notify();
     }
 }
+
+class LockExample {
+    Lock lock = new Lock() ;
+
+    /**
+     * 将在临界区内运行很长一段时间，
+     *
+     * @throws InterruptedException
+     */
+    public void doSynchronized() throws InterruptedException {
+        this.lock.lock();
+
+        // critical section, do a lot of work which takes a long time.
+
+        this.lock.unlock();
+    }
+}
+
